d8e1f1d07dc95d3f86fffafcd09977a19fd03942
RussiSunni <jonathandyason@hotmail.com>
2023-01-12 09:03:35 +0200

    dsad

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,16 +20,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+/*------------------------------------------
+--------------------------------------------
+Database Connection
+--------------------------------------------
+--------------------------------------------*/
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'C0gn1t1v3Psych0l0gy',
+  password: 'password',
+  database: 'visual-kalsbeek'
+});
+
+
+// Shows MariaDB Connect.
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('MariaDB connected...');
+});
+
+
+// Routes.
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
