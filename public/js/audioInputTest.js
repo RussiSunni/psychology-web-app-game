39d2e77a472982b62bba327f2e700a643ef0f000
RussiSunni <jonathandyason@hotmail.com>
2023-02-17 10:07:12 +0200

    Audio Input

--- public/js/audioInputTest.js
+++ public/js/audioInputTest.js
@@ -49,6 +49,54 @@ class AudioInputTest extends Phaser.Scene {
     }
 
     create() {
+
+        // Speech recognition --------------
+        // new instance of speech recognition
+        var recognition = new webkitSpeechRecognition();
+
+        // set params
+        recognition.continuous = true;
+        recognition.interimResults = true;
+        recognition.start();
+
+        recognition.onresult = function (event) {
+
+            // delve into words detected results & get the latest
+            // total results detected
+            var resultsLength = event.results.length - 1;
+            // get length of latest results
+            var ArrayLength = event.results[resultsLength].length - 1;
+            // get last word detected
+            var saidWord = event.results[resultsLength][ArrayLength].transcript;
+
+            // loop through links and match to word spoken
+            // for (i = 0; i < allLinks.length; i++) {
+
+            //     // get the word associated with the link
+            //     var dataWord = allLinks[i].dataset.word;
+
+            //     // if word matches chenge the colour of the link
+            //     if (saidWord.indexOf(dataWord) != -1) {
+            //         allLinks[i].style.color = 'red';
+            //     }
+            // }
+
+            // append the last word to the bottom sentence
+            // strongEl.innerHTML = saidWord;
+
+            console.log(saidWord);
+
+            // speech error handling
+            recognition.onerror = function (event) {
+                console.log('error?');
+                console.log(event);
+            }
+        }
+
+
+        // ----------------------------
+
+
         this.startTimedEvent = this.time.addEvent({ delay: 2000, callback: this.startEvent, callbackScope: this, loop: false });
 
         // Bar.
