e4d61a4ea7c044cad0264014b0b66bbdeefb814a
RussiSunni <jonathandyason@hotmail.com>
2023-02-20 18:52:35 +0200

    SSL cert

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const mysql = require('mysql');
 //var indexRouter = require('./routes/index');
 //var parametersRouter = require('./routes/parameters');
 
+// For SSL cert (HTTPS), required for microphone.
+var https = require('https');
+var http = require('http');
+const fs = require('fs')
+
 var app = express();
 
 // view engine setup
@@ -30,7 +35,7 @@ const conn = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'C0gn1t1v3Psych0l0gy',
-	//password: 'password',
+	password: 'password',
 	database: 'visual_kalsbeek'
 });
 
@@ -165,31 +170,31 @@ app.put('/instructions-3/edit', function (req, res, next) {
 });
 
 // Save game data.
-// app.post('/saveGameData', (req, res) => {
-// 	console.log("test")
+app.post('/saveGameData', (req, res) => {
+	console.log("test")
 
-// 	let sqlQuery = "INSERT INTO games SET ?";
+	let sqlQuery = "INSERT INTO games SET ?";
 
-// 	let data = {
-// 		date: new Date(),
-// 		task: req.body.task,
-// 		did_win: req.body.did_win,
-// 		left_climb_rate: req.body.left_climb_rate,
-// 		right_climb_rate: req.body.right_climb_rate,
-// 		left_drop_amount: req.body.left_drop_amount,
-// 		right_drop_amount: req.body.right_drop_amount,
-// 		left_delay_amount: req.body.left_delay_amount,
-// 		right_delay_amount: req.body.right_delay_amount,
-// 		left_penalty_amount: req.body.left_penalty_amount,
-// 		right_penalty_amount: req.body.right_penalty_amount
-// 	};
+	let data = {
+		date: new Date(),
+		task: req.body.task,
+		did_win: req.body.did_win,
+		left_climb_rate: req.body.left_climb_rate,
+		right_climb_rate: req.body.right_climb_rate,
+		left_drop_amount: req.body.left_drop_amount,
+		right_drop_amount: req.body.right_drop_amount,
+		left_delay_amount: req.body.left_delay_amount,
+		right_delay_amount: req.body.right_delay_amount,
+		left_penalty_amount: req.body.left_penalty_amount,
+		right_penalty_amount: req.body.right_penalty_amount
+	};
 
-// 	let query = conn.query(sqlQuery, data, (err, results) => {
-// 		if (err) throw err;
-// 		res.json(results);
-// 	});
+	let query = conn.query(sqlQuery, data, (err, results) => {
+		if (err) throw err;
+		res.json(results);
+	});
 
-// });
+});
 
 
 
@@ -260,6 +265,12 @@ function apiResponse(results) {
 Server listening
 --------------------------------------------
 --------------------------------------------*/
-app.listen(80, () => {
-	console.log('Server started on port 80...');
-});
+http.createServer(app).listen(80);
+https.createServer({
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.cert')
+}, app).listen(443);
+
+// app.listen(80, () => {
+// 	console.log('Server started on port 80...');
+// });
